eb4f43046a58f2e0ee3243fb5cc4ed989a895db2
Agustin Celentano <12614595+agustincelentano@users.noreply.github.com>
2025-11-14 15:19:07 -0300

    Update helm_base.sh with Prometheus commands and comments
    Added commands for reviewing Prometheus pods and permissions.

--- nullplatform-configuration-azure-aro/helm_base.sh
+++ nullplatform-configuration-azure-aro/helm_base.sh
@@ -1,8 +1,13 @@
 helm repo add nullplatform https://nullplatform.github.io/helm-charts
 helm repo update
 
-#######
+# Commands to upgrade helm chart base to configure promethus outside the terraform state
 helm list -n nullplatform-tools
 helm get values nullplatform-agent -n nullplatform-tools
 
 helm upgrade nullplatform-base nullplatform/nullplatform-base --namespace nullplatform-tools --version = 2.26.0 --reuse-values
+
+#Commands to review that pod and permissions for prometheus existis
+kubectl get podmonitors.monitoring.coreos.com -A
+kubectl get role -A | grep prometheus-k8s
+kubectl get RoleBinding  -A | grep prometheus-k8s
